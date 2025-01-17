0b361414a6aa98cab46526f059d0e2590d0b5cc0
alexander zillion <alex@alexzillion.com>
2025-01-16 21:37:41 -0800

    pull out magic string

--- lib/core/server/connection_manager.ml
+++ lib/core/server/connection_manager.ml
@@ -2,6 +2,7 @@ open Base
 
 type t = { clients : (string, Client.t) Hashtbl.t }
 
+let termination_message = "Connection terminated"
 let create () = { clients = Hashtbl.create (module String) }
 
 let add_client t client =
@@ -20,7 +21,7 @@ let drop_connection t client_id =
   | Some client ->
       (* Send close message and remove from clients *)
       Lwt.async (fun () ->
-          let* _ = client.send "Connection terminated" in
+          let* _ = client.send termination_message in
           match client.websocket with
           | Some websocket -> Dream.close_websocket websocket
           | None -> Lwt.return_unit);
@@ -36,7 +37,7 @@ let drop_user_connections t user_id =
       | Authenticated { user_id = uid; _ } ->
           if String.equal uid user_id then (
             Lwt.async (fun () ->
-                let* _ = client.send "Connection terminated" in
+                let* _ = client.send termination_message in
                 match client.websocket with
                 | Some websocket -> Dream.close_websocket websocket
                 | None -> Lwt.return_unit);
