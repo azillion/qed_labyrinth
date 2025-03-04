1765b63ad0a1d4a4b969cf4e1f7018723f0f2ca3
alexander zillion <alex@alexzillion.com>
2025-03-04 14:43:49 -0800

    add try around websocket handling

--- lib/server/websocket_handler.ml
+++ lib/server/websocket_handler.ml
@@ -12,25 +12,32 @@ let handler (state : Qed_domain.State.t) user_id websocket =
   Connection_manager.add_client state.connection_manager client;
 
   let rec process_messages () =
-    match%lwt Dream.receive websocket with
-    | Some msg -> (
-        let open Qed_domain.Protocol in
-        let yojson_msg = Yojson.Safe.from_string msg in
-        let message = client_message_of_yojson yojson_msg in
-        match message with
-        | Ok message -> (
-            match message with
-            | Command { command } ->
-                let message' = Qed_domain.Protocol.parse_command command in
-                let%lwt () = Queue.push state.client_message_queue message' client in
-                process_messages ()
-            | _ ->
-                let%lwt () = Queue.push state.client_message_queue message client in
-                process_messages ())
-        | Error err ->
-            ignore (Stdio.print_endline ("Parse error: " ^ err));
-            process_messages ())
-    | None ->
+    try
+      match%lwt Dream.receive websocket with
+      | Some msg -> (
+          let open Qed_domain.Protocol in
+          let yojson_msg = Yojson.Safe.from_string msg in
+          let message = client_message_of_yojson yojson_msg in
+          match message with
+          | Ok message -> (
+              match message with
+              | Command { command } ->
+                  let message' = Qed_domain.Protocol.parse_command command in
+                  let%lwt () = Infra.Queue.push state.client_message_queue { message = message'; client } in
+                  process_messages ()
+              | _ ->
+                  let%lwt () = Infra.Queue.push state.client_message_queue { message; client } in
+                  process_messages ())
+          | Error err ->
+              ignore (Stdio.print_endline ("Parse error: " ^ err));
+              process_messages ())
+      | None ->
+          Connection_manager.remove_client state.connection_manager client_id;
+          Lwt.return_unit
+    with
+    | exn ->
+        let error_msg = Exn.to_string exn in
+        ignore (Stdio.print_endline ("Websocket error: " ^ error_msg));
         Connection_manager.remove_client state.connection_manager client_id;
         Lwt.return_unit
   in
