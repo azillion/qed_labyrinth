c9e513f107dd7ea4caff814c39b2eb5cf3906552
alexander zillion <alex@alexzillion.com>
2025-03-14 22:23:01 -0700

    add event types

---
+++ lib/domain/event.ml
@@ -0,0 +1,14 @@
+type t =
+  | CreateCharacter of {
+      user_id: string;
+      name: string;
+      description: string;
+      starting_area_id: string;
+    }
+  | CharacterCreated of { user_id: string; character_id: string }
+  | CharacterCreationFailed of { user_id: string; error: string }
+  | SelectCharacter of { user_id: string; character_id: string }
+  | CharacterSelected of { user_id: string; character_id: string }
+  | CharacterSelectionFailed of { user_id: string; error: string }
+  | ListCharacters of { user_id: string }
+  | CharacterList of { user_id: string; characters: (string * string) list }
